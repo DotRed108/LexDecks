bbd04dfaddea68b5b57047e02f20a5b2b0f6fbe8
Ethan Dock <dockethan108@gmail.com>
2025-04-13 16:30:45 -0400

    cert testing

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 #[tokio::main]
 async fn main() {
     use std::path::PathBuf;
+    use std::env;
 
     use axum::Router;
     use axum_server::tls_rustls::RustlsConfig;
@@ -15,19 +16,8 @@ async fn main() {
     // let cert = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("cert.pem");
     // let key = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("key.pem");
 
-    let cert = "-----BEGIN CERTIFICATE-----\n
-    MIIBgTCCASagAwIBAgIUNG+YzU4l2hQxyYZ3dAZoUUTdLoAwCgYIKoZIzj0EAwIw
-    ITEfMB0GA1UEAwwWcmNnZW4gc2VsZiBzaWduZWQgY2VydDAgFw03NTAxMDEwMDAw
-    MDBaGA80MDk2MDEwMTAwMDAwMFowITEfMB0GA1UEAwwWcmNnZW4gc2VsZiBzaWdu
-    ZWQgY2VydDBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABHx8xymLUVZuVmyusTnj
-    HgpUId6h/YP794cF28vcEui2Opj14SO6NmwUMRqEB+FxxWNPIb3Z4oH4i/2Qb7Mw
-    Gf2jOjA4MDYGA1UdEQQvMC2CDGxleGxpbmd1YS5pb4INbGV4bGluZ3VhLm5ldIIO
-    bGV4LWxpbmd1YS5jb20wCgYIKoZIzj0EAwIDSQAwRgIhAK+ASIeXMIVkQc5zdeXs
-    fjkHoDqwpSGqG7RdzBM3dI8zAiEAnq6Ur2/CVD+6VVb7krzaqCymzmftf7x6JrCJ
-    GfofO2k=\n-----END CERTIFICATE-----".as_bytes().into();
-    let key = "-----BEGIN PRIVATE KEY-----\nMIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgMn676qIz9iiNcckt
-    gzmBQzoTU7SLxs5OaWlxZiC/miGhRANCAAR8fMcpi1FWblZsrrE54x4KVCHeof2D
-    +/eHBdvL3BLotjqY9eEjujZsFDEahAfhccVjTyG92eKB+Iv9kG+zMBn9\n-----END PRIVATE KEY-----".as_bytes().into();
+    let cert = env::var("SSL_CERT").unwrap().as_bytes().into();
+    let key = env::var("SSL_CERT_PRIVATE_KEY").unwrap().as_bytes().into();
 
     let config = RustlsConfig::from_pem(cert, key).await.unwrap();
 
